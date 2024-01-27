c5cd7d60ed8878f93bb77e1276c9cf49d115fef6
Tuyzera <utpadelarthur@gmail.com>
2024-01-27 13:46:56 -0300

    First commit of the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import {FormControl, InputLabel, MenuItem, Select, TextField} from '@mui/material'
+
 import './App.css';
 
 function App() {
+  const [filters, setFilters] = useState([])
+  const [filterSelected, setFilterSelected] = useState("")
+
+
+  function handleChangeFilterValue(event: any){
+    setFilterSelected(event.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='container-title'>
+        <h1>Lista de Tarefas</h1>
+      </div>
+      <div className='container-search'>
+          <TextField id="outlined-basic" label="Pesquisar" variant="outlined" />
+      </div>
+      <div className='container-filter'>
+        <div className='container-filter-right'>
+          <h1>Filtro</h1>
+          <FormControl sx={{m: 1, width: 150}}>
+            <InputLabel id="demo-simple-select-label">Categorias</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={filterSelected}
+                label="Filtro"
+                onChange={handleChangeFilterValue}
+              >
+                <MenuItem value={"Todas"} >Todas</MenuItem>
+                <MenuItem value={"Trabalho"}>Trabalho</MenuItem>
+                <MenuItem value={"Estudos"}>Estudos</MenuItem>
+                <MenuItem value={"Academia"}>Academia</MenuItem>
+              </Select>
+</FormControl>
+        </div>
+        <div className='container-filter-left'>
+          <label>Ordem Alfabetica</label>
+        </div>
+         
+      </div>
     </div>
   );
 }
